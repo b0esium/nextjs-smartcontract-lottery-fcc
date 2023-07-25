1a5cbdbbace4c4c7814b98cbda5e94b0eb19096e
b0esium <b0esium@protonmail.com>
2023-07-25 15:49:03 +0200

    Initial commit

---
+++ components/Header.jsx
@@ -0,0 +1,11 @@
+import { ConnectButton } from "web3uikit"
+const Header = () => {
+    return (
+        <div>
+            Decentralized Lottery
+            <ConnectButton moralisAuth={false} />
+        </div>
+    )
+}
+
+export default Header
---
+++ components/LotteryEntrance.jsx
@@ -0,0 +1,9 @@
+import { useWeb3Contract } from "react-moralis"
+
+const LotteryEntrance = () => {
+    // const { runContractFunction: enterRaffle} = useWeb3Contract({abi:,contractAddress:,functionName:, params: {}, msgValue: })
+
+    return <div>Hi!</div>
+}
+
+export default LotteryEntrance
