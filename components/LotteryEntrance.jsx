05692ae83dfb2014926bc5bd92b8acba655b59b9
b0esium <b0esium@protonmail.com>
2023-07-25 18:17:32 +0200

    Add LotteryEntrance component

--- components/LotteryEntrance.jsx
+++ components/LotteryEntrance.jsx
@@ -1,9 +1,43 @@
 import { useWeb3Contract } from "react-moralis"
+import { abi, contractAddresses } from "../constants"
+import { useMoralis } from "react-moralis"
+import { useEffect, useState } from "react"
+import { ethers } from "ethers"
 
 const LotteryEntrance = () => {
-    // const { runContractFunction: enterRaffle} = useWeb3Contract({abi:,contractAddress:,functionName:, params: {}, msgValue: })
+    const { chainId: chainIdHex, isWeb3Enabled } = useMoralis()
+    const chainId = parseInt(chainIdHex)
+    const raffleAddress = chainId in contractAddresses ? contractAddresses[chainId][0] : null
 
-    return <div>Hi!</div>
+    const [entranceFee, setEntranceFee] = useState("0")
+
+    // const { runContractFunction: enterRaffle} = useWeb3Contract({abi: abi,contractAddress:raffleAddress,functionName:"enterRaffle", params: {}, msgValue: })
+    const { runContractFunction: getEntranceFee } = useWeb3Contract({
+        abi: abi,
+        contractAddress: raffleAddress,
+        functionName: "getEntranceFee",
+        params: {},
+    })
+
+    useEffect(() => {
+        if (isWeb3Enabled) {
+            async function updateUI() {
+                const entranceFeeFromCall = await getEntranceFee()
+                const entranceFeeString = entranceFeeFromCall._hex
+                entranceFeeString = ethers.utils.formatUnits(entranceFeeString, "ether")
+                setEntranceFee(entranceFeeString)
+                console.log(entranceFee)
+                console.log(typeof entranceFee)
+            }
+            updateUI()
+        }
+    }, [isWeb3Enabled])
+
+    return (
+        <div>
+            Entrance fee: <div>{entranceFee} ETH</div>
+        </div>
+    )
 }
 
 export default LotteryEntrance
